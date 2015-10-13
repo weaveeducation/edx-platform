891eac66b6d19fb055662671d9702591c0e46623
aamir-khan <aamir@edx.org>
2015-10-13 18:06:53 +0500

    ECOM-2430: Added the tracking GA events on dashboard

---
+++ lms/static/js/dashboard/track_events.js
@@ -0,0 +1,91 @@
+/**
+ * Track interaction with the student dashboard..
+ */
+
+var edx = edx || {};
+
+(function ($) {
+    'use strict';
+
+    edx.dashboard = edx.dashboard || {};
+
+    edx.dashboard.TrackEvents = function() {
+
+        // Emit an event when the "course title link" is clicked.
+        $(".course-title-link").on("click", function (event) {
+            var courseKey = $(event.target).data("course-key");
+            window.analytics.track(
+                "edx.bi.dashboard.course_title.clicked",
+                {
+                    category: "dashboard",
+                    label: courseKey
+                }
+            );
+        });
+
+        // Emit an event  when the "course image" is clicked.
+        $(".dashboard-course-image").on("click", function (event) {
+            var courseKey = $(event.target).closest(".dashboard-course-image").data("course-key");
+            window.analytics.track(
+                "edx.bi.dashboard.course_image.clicked",
+                {
+                    category: "dashboard",
+                    label: courseKey
+                }
+            );
+        });
+
+        // Emit an event  when the "View Course" button is clicked.
+        $(".enter-course-link").on("click", function (event) {
+            var courseKey = $(event.target).data("course-key");
+            window.analytics.track(
+                "edx.bi.dashboard.enter_course.clicked",
+                {
+                    category: "dashboard",
+                    label: courseKey
+                }
+            );
+        });
+
+        // Emit an event when the options dropdown is engaged.
+        $(".wrapper-action-more").on("click", function (event) {
+            var courseKey = $(event.target).closest(".wrapper-action-more").data("course-key");
+            window.analytics.track(
+                "edx.bi.dashboard.more_action_button.clicked",
+                {
+                    category: "dashboard",
+                    label: courseKey
+                }
+            );
+        });
+
+        // Emit an event  when the "Learn about verified" link is clicked.
+        $(".learned-verified-track").on("click", function (event) {
+            var courseKey = $(event.target).data("course-key");
+            window.analytics.track(
+                "edx.bi.dashboard.learn_verified.clicked",
+                {
+                    category: "dashboard",
+                    label: courseKey
+                }
+            );
+        });
+
+        // Emit an event  when the "Find Courses" button is clicked.
+        $(".btn-find-courses").on("click", function () {
+            window.analytics.track(
+                "edx.bi.dashboard.find_course_button.clicked",
+                {
+                    category: "dashboard",
+                    label: "find_course_button"
+                }
+            );
+        });
+    };
+
+    $(document).ready(function() {
+
+        edx.dashboard.TrackEvents();
+
+    });
+})(jQuery);
---
+++ lms/static/js/spec/dashboard/track_events.js
@@ -0,0 +1,95 @@
+define([
+        'jquery',
+        'js/dashboard/track_events'
+    ],
+    function($) {
+        'use strict';
+        var edx = edx || {};
+
+        describe("edx.dashboard.TrackEvents", function() {
+
+            var COURSE_ID = "edX/DemoX/Demo_Course";
+            var CATEGORY = "dashboard";
+
+            beforeEach(function() {
+                // Stub the analytics event tracker
+                window.analytics = jasmine.createSpyObj('analytics', ['track']);
+                loadFixtures('js/fixtures/dashboard/dashboard.html');
+                edx.dashboard.TrackEvents();
+            });
+
+            it("sends an analytics event when the user clicks course title link", function() {
+                $(".course-title-link").click();
+                // Verify that analytics events fire when the "course title link" is clicked.
+                expect(window.analytics.track).toHaveBeenCalledWith(
+                    "edx.bi.dashboard.course_title.clicked",
+                    {
+                        category: CATEGORY,
+                        label: COURSE_ID
+                    }
+                );
+            });
+
+            it("sends an analytics event when the user clicks course image link", function() {
+                $(".dashboard-course-image").click();
+                // Verify that analytics events fire when the "course image link" is clicked.
+                expect(window.analytics.track).toHaveBeenCalledWith(
+                    "edx.bi.dashboard.course_image.clicked",
+                    {
+                        category: CATEGORY,
+                        label: COURSE_ID
+                    }
+                );
+            });
+
+
+            it("sends an analytics event when the user clicks enter course link", function() {
+                $(".enter-course-link").click();
+                // Verify that analytics events fire when the "enter course link" is clicked.
+                expect(window.analytics.track).toHaveBeenCalledWith(
+                    "edx.bi.dashboard.enter_course.clicked",
+                    {
+                        category: CATEGORY,
+                        label: COURSE_ID
+                    }
+                );
+            });
+
+            it("sends an analytics event when the user clicks enter course link", function() {
+                $(".wrapper-action-more").click();
+                // Verify that analytics events fire when the "Settings" button is clicked.
+                expect(window.analytics.track).toHaveBeenCalledWith(
+                    "edx.bi.dashboard.more_action_button.clicked",
+                    {
+                        category: CATEGORY,
+                        label: COURSE_ID
+                    }
+                );
+            });
+
+            it("sends an analytics event when the user clicks the learned about verified track link", function() {
+                $(".learned-verified-track").click();
+                //Verify that analytics events fire when the "Learned about verified track" link is clicked.
+                expect(window.analytics.track).toHaveBeenCalledWith(
+                    "edx.bi.dashboard.learn_verified.clicked",
+                    {
+                        category: CATEGORY,
+                        label: COURSE_ID
+                    }
+                );
+            });
+
+            it("sends an analytics event when the user clicks find courses button", function() {
+                $(".btn-find-courses").click();
+                // Verify that analytics events fire when the "user clicks find the course" button.
+                expect(window.analytics.track).toHaveBeenCalledWith(
+                    "edx.bi.dashboard.find_course_button.clicked",
+                    {
+                        category: CATEGORY,
+                        label: 'find_course_button'
+                    }
+                );
+            });
+        });
+    }
+);
