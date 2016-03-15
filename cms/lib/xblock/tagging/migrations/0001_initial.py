5ad15d5b97266720b248b1f7fa02ab402bb9461a
Dmitry Viskov <dmitry.viskov@webenterprise.ru>
2016-03-15 19:13:35 +0300

    Rollback tagging functionality. Dynamic values for the selectboxes with tags (tags are stored in the database tables) Tags are global. And are presented without UI window for edit.

--- cms/lib/xblock/tagging/migrations/0001_initial.py
+++ cms/lib/xblock/tagging/migrations/0001_initial.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-import xmodule_django.models
 
 
 class Migration(migrations.Migration):
@@ -12,19 +11,29 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='AvailableTags',
+            name='TagAvailableValues',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('course_id', xmodule_django.models.CourseKeyField(max_length=255, db_index=True)),
-                ('category', models.CharField(db_index=True, max_length=32, choices=[(b'difficulty_tag', b'difficulty_tag'), (b'learning_outcome_tag', b'learning_outcome_tag')])),
-                ('tag', models.CharField(max_length=255)),
+                ('value', models.CharField(max_length=255)),
             ],
             options={
                 'ordering': ('id',),
             },
         ),
-        migrations.AlterUniqueTogether(
-            name='availabletags',
-            unique_together=set([('course_id', 'category', 'tag')]),
+        migrations.CreateModel(
+            name='TagCategories',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('title', models.CharField(max_length=255)),
+            ],
+            options={
+                'ordering': ('title',),
+            },
+        ),
+        migrations.AddField(
+            model_name='tagavailablevalues',
+            name='category',
+            field=models.ForeignKey(to='tagging.TagCategories'),
         ),
     ]
--- cms/lib/xblock/tagging/tagging.py
+++ cms/lib/xblock/tagging/tagging.py
@@ -8,71 +8,13 @@ from xblock.fragment import Fragment
 from xblock.fields import Scope, Dict
 from xmodule.x_module import STUDENT_VIEW
 from xmodule.capa_module import CapaModule
-from abc import ABCMeta, abstractproperty
 from edxmako.shortcuts import render_to_string
 from django.conf import settings
-from django.db import transaction
 from webob import Response
-from .models import AvailableTags
+from .models import TagCategories
 
 
 _ = lambda text: text
-
-
-class AbstractTag(object):
-    """
-    Abstract class for tags
-    """
-    __metaclass__ = ABCMeta
-
-    @abstractproperty
-    def key(self):
-        """
-        Subclasses must implement key
-        """
-        raise NotImplementedError('Subclasses must implement key')
-
-    @abstractproperty
-    def name(self):
-        """
-        Subclasses must implement name
-        """
-        raise NotImplementedError('Subclasses must implement name')
-
-    def get_values(self, course_id):
-        """ Allowed values for the selector """
-        tags = [v.tag for v in AvailableTags.objects.filter(course_id=course_id, category=self.key)]
-        return tags[:]
-
-
-class DifficultyTag(AbstractTag):
-    """
-    Particular implementation tags for difficulty
-    """
-    @property
-    def key(self):
-        """ Identifier for the difficulty selector """
-        return 'difficulty_tag'
-
-    @property
-    def name(self):
-        """ Label for the difficulty selector """
-        return _('Difficulty')
-
-
-class LearningOutcomeTag(AbstractTag):
-    """
-    Particular implementation tags for learning outcomes
-    """
-    @property
-    def key(self):
-        """ Identifier for the learning outcome selector """
-        return 'learning_outcome_tag'
-
-    @property
-    def name(self):
-        """ Label for the learning outcome selector """
-        return _('Learning outcomes')
 
 
 class StructuredTagsAside(XBlockAside):
@@ -82,7 +24,12 @@ class StructuredTagsAside(XBlockAside):
     saved_tags = Dict(help=_("Dictionary with the available tags"),
                       scope=Scope.content,
                       default={},)
-    available_tags = [DifficultyTag(), LearningOutcomeTag()]
+
+    def get_available_tags(self):
+        """
+        Return available tags
+        """
+        return TagCategories.objects.all()
 
     def _get_studio_resource_url(self, relative_url):
         """
@@ -96,19 +43,18 @@ class StructuredTagsAside(XBlockAside):
         Display the tag selector with specific categories and allowed values,
         depending on the context.
         """
-        course_key = self.scope_ids.usage_id.course_key
         if isinstance(block, CapaModule):
             tags = []
-            for tag in self.available_tags:
-                values = tag.get_values(course_key)
-                current_value = self.saved_tags.get(tag.key, None)
+            for tag in self.get_available_tags():
+                values = tag.get_values()
+                current_value = self.saved_tags.get(tag.name, None)
 
                 if current_value is not None and current_value not in values:
                     values.insert(0, current_value)
 
                 tags.append({
-                    'key': tag.key,
-                    'title': tag.name,
+                    'key': tag.name,
+                    'title': tag.title,
                     'values': values,
                     'current_value': current_value
                 })
@@ -130,63 +76,16 @@ class StructuredTagsAside(XBlockAside):
 
         tag = request.params['tag'].split(':')
 
-        course_key = self.scope_ids.usage_id.course_key
-        for av_tag in self.available_tags:
-            if av_tag.key == tag[0]:
-                if tag[1] in av_tag.get_values(course_key):
-                    self.saved_tags[tag[0]] = tag[1]
-                    found = True
-                elif tag[1] == '':
+        for av_tag in self.get_available_tags():
+            if av_tag.name == tag[0]:
+                if tag[1] == '':
                     self.saved_tags[tag[0]] = None
+                    found = True
+                elif tag[1] in av_tag.get_values():
+                    self.saved_tags[tag[0]] = tag[1]
                     found = True
 
         if not found:
             return Response("Invalid 'tag' parameter", status=400)
 
         return Response()
-
-    @XBlock.handler
-    def edit_tags_view(self, request=None, suffix=None):  # pylint: disable=unused-argument
-        course_key = self.scope_ids.usage_id.course_key
-
-        tags = []
-        for tag in self.available_tags:
-            tags.append({
-                'key': tag.key,
-                'title': tag.name,
-                'values': '\n'.join(tag.get_values(course_key))
-            })
-
-        data = {
-            'html': render_to_string('structured_tags_block_editor.html', {'tags': tags})
-        }
-        return Response(json=data)
-
-    @XBlock.handler
-    def update_values(self, request=None, suffix=None):  # pylint: disable=unused-argument
-        course_key = self.scope_ids.usage_id.course_key
-
-        tags_dict = {}
-        for available_tag in self.available_tags:
-            tags_dict[available_tag.key] = available_tag
-
-        with transaction.atomic():
-            for tag_key in request.POST:
-                tag = tags_dict.get(tag_key, None)
-                if tag:
-                    new_tag_values = []
-                    tmp_list = [v for v in request.POST[tag_key].splitlines() if v.strip()]
-                    for tmp in tmp_list:
-                        val = tmp.strip()
-                        if val not in new_tag_values:
-                            new_tag_values.append(val)
-                    if new_tag_values:
-                        self._update_tag(course_key, tag, new_tag_values)
-
-        return Response()
-
-    def _update_tag(self, course_id, tag, data):
-        AvailableTags.objects.filter(course_id=course_id, category=tag.key).delete()
-        for value in data:
-            AvailableTags(course_id=course_id, category=tag.key, tag=value).save()
-
