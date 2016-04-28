84483dc242d1990eef7804c686014061c005f53b
Dmitry Viskov <dmitry.viskov@webenterprise.ru>
2016-04-28 03:51:44 +0300

    EDX-105 - "Retype password" field

--- common/djangoapps/student/forms.py
+++ common/djangoapps/student/forms.py
@@ -106,6 +106,7 @@ class TrueField(forms.BooleanField):
 _USERNAME_TOO_SHORT_MSG = _("Username must be minimum of two characters long")
 _EMAIL_INVALID_MSG = _("A properly formatted e-mail is required")
 _PASSWORD_INVALID_MSG = _("A valid password is required")
+_PASSWORD_COPY_INVALID_MSG = _("A copy of password is required")
 _NAME_TOO_SHORT_MSG = _("Your legal name must be a minimum of two characters long")
 
 
@@ -138,6 +139,13 @@ class AccountCreationForm(forms.Form):
         error_messages={
             "required": _PASSWORD_INVALID_MSG,
             "min_length": _PASSWORD_INVALID_MSG,
+        }
+    )
+    password_copy = forms.CharField(
+        min_length=2,
+        error_messages={
+            "required": _PASSWORD_COPY_INVALID_MSG,
+            "min_length": _PASSWORD_COPY_INVALID_MSG,
         }
     )
     name = forms.CharField(
@@ -224,6 +232,15 @@ class AccountCreationForm(forms.Form):
             except ValidationError, err:
                 raise ValidationError(_("Password: ") + "; ".join(err.messages))
         return password
+
+    def clean_password_copy(self):
+        """Enforce password policies (if applicable)"""
+        password_copy = self.cleaned_data["password_copy"]
+
+        if "password" in self.cleaned_data and self.cleaned_data["password"] != password_copy:
+            raise ValidationError(_("Passwords don't match"))
+
+        return password_copy
 
     def clean_year_of_birth(self):
         """
