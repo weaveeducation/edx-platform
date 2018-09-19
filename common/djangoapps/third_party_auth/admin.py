510ba604db733e4fcc514e03696f83edefcf9f2d
Dmitry Viskov <dmitry.viskov@webenterprise.ru>
2018-09-20 00:24:32 +0300

    EDX-231 - SSO by Microsite

--- common/djangoapps/third_party_auth/admin.py
+++ common/djangoapps/third_party_auth/admin.py
@@ -18,6 +18,7 @@ from .models import (
     OAuth2ProviderConfig,
     ProviderApiPermissions,
     SAMLConfiguration,
+    SAMLConfigurationPerMicrosite,
     SAMLProviderConfig,
     SAMLProviderData
 )
@@ -42,6 +43,14 @@ class OAuth2ProviderConfigAdmin(KeyedConfigurationModelAdmin):
         )
 
 admin.site.register(OAuth2ProviderConfig, OAuth2ProviderConfigAdmin)
+
+
+class SAMLConfigurationPerMicrositeAdmin(admin.ModelAdmin):
+    """ Django Admin class for SAMLConfigurationPerMicrosite """
+    list_display = ('id', 'domain', 'entity_id')
+
+
+admin.site.register(SAMLConfigurationPerMicrosite, SAMLConfigurationPerMicrositeAdmin)
 
 
 class SAMLProviderConfigForm(forms.ModelForm):
