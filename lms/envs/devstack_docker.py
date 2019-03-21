290b1cf1e7505bda1f3b4a106d10ebfa34af59e5
Dmitry <dmitry.viskov@webenterprise.ru>
2019-03-21 23:39:31 +0300

    x-forwarded-for headers

--- lms/envs/devstack_docker.py
+++ lms/envs/devstack_docker.py
@@ -83,3 +83,6 @@ MKTG_URLS = {
 CREDENTIALS_SERVICE_USERNAME = 'credentials_worker'
 
 COURSE_CATALOG_API_URL = 'http://edx.devstack.discovery:18381/api/v1/'
+
+USE_X_FORWARDED_HOST = True
+USE_X_FORWARDED_PORT = True
