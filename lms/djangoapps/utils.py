bbaf4edb8de799cc8712258fbf6d0ede6f1c0d2c
Oleg Laputin <oleg.laputin@epicsoftwaredev.com>
2019-05-03 14:57:15 +0300

    Update generating of new edx user based on existed.

--- lms/djangoapps/utils.py
+++ lms/djangoapps/utils.py
@@ -2,6 +2,13 @@
 Helper Methods
 """
 
+from django.contrib.auth.models import User
+from django.db.models import Q
+from django.db import transaction
+from django.db.utils import IntegrityError
+
+USERNAME_DB_FIELD_SIZE = 30
+EMAIL_DB_FIELD_SIZE = 254
 
 def _get_key(key_or_id, key_cls):
     """
@@ -13,3 +20,41 @@ def _get_key(key_or_id, key_cls):
         if isinstance(key_or_id, basestring)
         else key_or_id
     )
+
+
+def _create_edx_user(email, username, password, user=None):
+    i = 1
+    new_email = email
+    new_username = username
+
+    if user is not None:
+        new_email, new_username = _get_new_email_and_username(user, new_email, new_username, i)
+        i = i + 1
+
+    while True:
+        try:
+            with transaction.atomic():
+                return User.objects.create_user(
+                    email=new_email,
+                    username=new_username,
+                    password=password
+                )
+        except IntegrityError:
+            ex_user = User.objects.get(Q(email=new_email) | Q(username=new_username))
+            new_email, new_username = _get_new_email_and_username(ex_user, new_email, new_username, i)
+            i = i + 1
+
+
+def _get_new_email_and_username(ex_user, new_email, new_username, num):
+    if ex_user.email == new_email:
+        if len(ex_user.email) > (EMAIL_DB_FIELD_SIZE - 3):
+            new_email = ex_user.email[0:EMAIL_DB_FIELD_SIZE - 3] + str(num)
+        else:
+            new_email = ex_user.email + str(num)
+    if ex_user.username.lower() == new_username.lower():
+        if len(ex_user.username) > (USERNAME_DB_FIELD_SIZE - 3):
+            new_username = new_username[0:USERNAME_DB_FIELD_SIZE - 3] + str(num)
+        else:
+            new_username = new_username + str(num)
+
+    return new_email, new_username
