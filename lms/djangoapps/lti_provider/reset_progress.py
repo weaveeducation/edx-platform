b90d724c26c385846be25a4340aba7dcbe6ad17f
dmitry-viskov <dmitry.viskov@webenterprise.ru>
2019-09-30 12:16:46 +0300

    DEV-371 - Investigate - multiple attempts overrides LMS gradebook

--- lms/djangoapps/lti_provider/reset_progress.py
+++ lms/djangoapps/lti_provider/reset_progress.py
@@ -1,12 +1,15 @@
+import hashlib
+
 from django.db import transaction
 from django.db.utils import IntegrityError
 
 from lms.djangoapps.instructor_task.tasks_helper.module_state import update_reset_progress
-from lti_provider.models import LtiContextId, LTI1p1
+from lti_provider.models import GradedAssignment, LtiContextId, LTI1p1
 from xmodule.modulestore.django import modulestore
 
 
-def check_and_reset_lti_user_progress(context_id, user, course_key, usage_key, lti_version=None):
+def check_and_reset_lti_user_progress(context_id, user, course_key, usage_key,
+                                      lis_result_sourcedid=None, lti_version=None):
     if not lti_version:
         lti_version = LTI1p1
     if context_id:
@@ -23,6 +26,11 @@ def check_and_reset_lti_user_progress(context_id, user, course_key, usage_key, l
                 with modulestore().bulk_operations(course_key):
                     block = modulestore().get_item(usage_key)
                     update_reset_progress(user, course_key, block)
+                    if lti_version == LTI1p1 and lis_result_sourcedid:
+                        lis_result_sourcedid_hash = hashlib.md5(str(lis_result_sourcedid)).hexdigest()
+                        GradedAssignment.objects.filter(
+                            course_key=course_key, usage_key=usage_key, user=user
+                        ).exclude(lis_result_sourcedid=lis_result_sourcedid_hash).delete()
         except LtiContextId.DoesNotExist:
             try:
                 with transaction.atomic():
