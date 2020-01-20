ded7e84413e688f6ede1f7e0557e7aa0da162f7e
dmitry-viskov <dmitry.viskov@webenterprise.ru>
2020-01-20 12:21:27 +0300

    DEV-429 - Enable LTI Access to the course level My Skills report

--- common/djangoapps/credo_modules/migrations/0033_organizationtype_my_skills_page_lti_access.py
+++ common/djangoapps/credo_modules/migrations/0033_organizationtype_my_skills_page_lti_access.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('credo_modules', '0031_tagdescription'),
+        ('credo_modules', '0032_tagdescription'),
     ]
 
     operations = [
