fb780f3f3e3944d31729f34b82f2d2acf85d0738
dmitry-viskov <dmitry.viskov@webenterprise.ru>
2020-09-01 18:14:03 +0300

    Instructor dashboard optimizations

--- lms/djangoapps/instructor/views/instructor_dashboard.py
+++ lms/djangoapps/instructor/views/instructor_dashboard.py
@@ -115,6 +115,16 @@ def instructor_dashboard_2(request, course_id):
     try:
         course_key = CourseKey.from_string(course_id)
     except InvalidKeyError:
+        log.error("Unable to find course with course key %s while loading the Instructor Dashboard.", course_id)
+        return HttpResponseServerError()
+    with modulestore().bulk_operations(course_key):
+        return _get_instructor_dashboard_2(request, course_id)
+
+
+def _get_instructor_dashboard_2(request, course_id):
+    try:
+        course_key = CourseKey.from_string(course_id)
+    except InvalidKeyError:
         log.error(u"Unable to find course with course key %s while loading the Instructor Dashboard.", course_id)
         return HttpResponseServerError()
 
