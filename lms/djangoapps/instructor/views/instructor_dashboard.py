8da2eff31610d0451059272bee5489d6382a14ac
dmitry-viskov <dmitry.viskov@webenterprise.ru>
2020-09-03 01:28:12 +0300

    Bugfix

--- lms/djangoapps/instructor/views/instructor_dashboard.py
+++ lms/djangoapps/instructor/views/instructor_dashboard.py
@@ -857,7 +857,10 @@ def _section_open_response_assessment(request, course, openassessment_blocks, ac
         result_item_id = six.text_type(block.location)
         if result_item_id not in parents:
             vert, seq, chapter = _get_full_path_names(block)
-            parents[result_item_id] = (vert, seq, chapter)
+            if vert and seq and chapter:
+                parents[result_item_id] = (vert, seq, chapter)
+            else:
+                continue
         assessment_name = _("Team") + " : " + block.display_name if block.teams_enabled else block.display_name
         ora_items.append({
             'id': result_item_id,
@@ -932,9 +935,13 @@ def _section_credo_insights(request, course):
 
 
 def _get_full_path_names(block):
-    vert = modulestore().get_item(block.parent)
-    seq = modulestore().get_item(vert.parent)
-    chapter = modulestore().get_item(seq.parent)
+    vert, seq, chapter = None, None, None
+    if block.parent:
+        vert = modulestore().get_item(block.parent)
+    if vert.parent:
+        seq = modulestore().get_item(vert.parent)
+    if seq.parent:
+        chapter = modulestore().get_item(seq.parent)
     return vert, seq, chapter
 
 
