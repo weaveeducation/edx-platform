140657c891dcc0126b32fa13edb505c834c713ec
dmitry-viskov <dmitry.viskov@webenterprise.ru>
2020-10-12 14:27:52 +0300

    Remove useless code

--- openedx/features/discounts/utils.py
+++ openedx/features/discounts/utils.py
@@ -105,43 +105,43 @@ def generate_offer_html(user, course):
     Returns a openedx.core.djangolib.markup.HTML object, or None if the user
     should not be shown an offer message.
     """
-    if user and not user.is_anonymous and course:
-        now = datetime.now(tz=pytz.UTC).strftime(u"%Y-%m-%d %H:%M:%S%z")
-        saw_banner = ExperimentData.objects.filter(
-            user=user, experiment_id=REV1008_EXPERIMENT_ID, key=str(course)
-        )
-        if not saw_banner:
-            ExperimentData.objects.create(
-                user=user, experiment_id=REV1008_EXPERIMENT_ID, key=str(course), value=now
-            )
-        discount_expiration_date = get_discount_expiration_date(user, course)
-        if (discount_expiration_date and
-                can_receive_discount(user=user, course=course, discount_expiration_date=discount_expiration_date)):
-            # Translator: xgettext:no-python-format
-            offer_message = _(u'{banner_open} Upgrade by {discount_expiration_date} and save {percentage}% '
-                              u'[{strikeout_price}]{span_close}{br}Use code {b_open}{code}{b_close} at checkout! '
-                              u'{a_open}Upgrade Now{a_close}{div_close}')
-
-            message_html = HTML(offer_message).format(
-                a_open=HTML(u'<a href="{upgrade_link}">').format(
-                    upgrade_link=verified_upgrade_deadline_link(user=user, course=course)
-                ),
-                a_close=HTML('</a>'),
-                b_open=HTML('<b>'),
-                code=Text('BIENVENIDOAEDX') if get_language() == 'es-419' else Text('EDXWELCOME'),
-                b_close=HTML('</b>'),
-                br=HTML('<br>'),
-                banner_open=HTML(
-                    '<div class="first-purchase-offer-banner" role="note">'
-                    '<span class="first-purchase-offer-banner-bold">'
-                ),
-                discount_expiration_date=discount_expiration_date.strftime(u'%B %d'),
-                percentage=discount_percentage(course),
-                span_close=HTML('</span>'),
-                div_close=HTML('</div>'),
-                strikeout_price=HTML(format_strikeout_price(user, course, check_for_discount=False)[0])
-            )
-            return message_html
+    #if user and not user.is_anonymous and course:
+    #    now = datetime.now(tz=pytz.UTC).strftime(u"%Y-%m-%d %H:%M:%S%z")
+    #    saw_banner = ExperimentData.objects.filter(
+    #        user=user, experiment_id=REV1008_EXPERIMENT_ID, key=str(course)
+    #    )
+    #    if not saw_banner:
+    #        ExperimentData.objects.create(
+    #            user=user, experiment_id=REV1008_EXPERIMENT_ID, key=str(course), value=now
+    #        )
+    #    discount_expiration_date = get_discount_expiration_date(user, course)
+    #    if (discount_expiration_date and
+    #            can_receive_discount(user=user, course=course, discount_expiration_date=discount_expiration_date)):
+    #        # Translator: xgettext:no-python-format
+    #        offer_message = _(u'{banner_open} Upgrade by {discount_expiration_date} and save {percentage}% '
+    #                          u'[{strikeout_price}]{span_close}{br}Use code {b_open}{code}{b_close} at checkout! '
+    #                          u'{a_open}Upgrade Now{a_close}{div_close}')
+    #
+    #        message_html = HTML(offer_message).format(
+    #            a_open=HTML(u'<a href="{upgrade_link}">').format(
+    #                upgrade_link=verified_upgrade_deadline_link(user=user, course=course)
+    #            ),
+    #            a_close=HTML('</a>'),
+    #            b_open=HTML('<b>'),
+    #            code=Text('BIENVENIDOAEDX') if get_language() == 'es-419' else Text('EDXWELCOME'),
+    #            b_close=HTML('</b>'),
+    #            br=HTML('<br>'),
+    #            banner_open=HTML(
+    #                '<div class="first-purchase-offer-banner" role="note">'
+    #                '<span class="first-purchase-offer-banner-bold">'
+    #            ),
+    #            discount_expiration_date=discount_expiration_date.strftime(u'%B %d'),
+    #            percentage=discount_percentage(course),
+    #            span_close=HTML('</span>'),
+    #            div_close=HTML('</div>'),
+    #            strikeout_price=HTML(format_strikeout_price(user, course, check_for_discount=False)[0])
+    #        )
+    #        return message_html
     return None
 
 
