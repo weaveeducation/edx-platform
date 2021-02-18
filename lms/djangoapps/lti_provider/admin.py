0045f310adf343e80c7dd1fab5d2de4c1984419b
dmitry-viskov <dmitry.viskov@webenterprise.ru>
2021-02-18 16:11:03 +0300

    DEV-855 - Add models CredoModulesUserProfile, CredoStudentProperties, SendScores, TrackingLogConfig, PropertiesInfo, LtiUser, LtiContextId, GradedAssignment into LMS django admin

--- lms/djangoapps/lti_provider/admin.py
+++ lms/djangoapps/lti_provider/admin.py
@@ -4,7 +4,7 @@ Admin interface for LTI Provider app.
 
 from django.contrib import admin
 
-from .models import LtiConsumer, OutcomeService
+from .models import LtiConsumer, OutcomeService, LtiUser, LtiContextId, GradedAssignment
 
 
 class LtiConsumerAdmin(admin.ModelAdmin):
@@ -38,5 +38,38 @@ class OutcomeServiceAdmin(admin.ModelAdmin):
         return []
 
 
+class ReadOnlyMixin(object):
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+class LtiUserAdmin(ReadOnlyMixin, admin.ModelAdmin):
+    list_display = ('id', 'lti_consumer_info', 'lti_user_id', 'edx_user')
+    search_fields = ['edx_user__username', 'edx_user__email', 'lti_user_id', 'lti_consumer__consumer_name']
+
+    def lti_consumer_info(self, obj):
+        return obj.lti_consumer.consumer_name + ' [id=' + str(obj.lti_consumer.id) + ']'
+
+
+class LtiContextIdAdmin(ReadOnlyMixin, admin.ModelAdmin):
+    list_display = ('id', 'user', 'course_key', 'usage_key', 'lti_version', 'value', 'properties')
+    search_fields = ['user__username', 'user__email', 'course_key', 'usage_key']
+
+
+class GradedAssignmentAdmin(ReadOnlyMixin, admin.ModelAdmin):
+    list_display = ('id', 'user', 'course_key', 'usage_key', 'outcome_service_id',
+                    'lis_result_sourcedid', 'lis_result_sourcedid_value')
+    search_fields = ['user__username', 'user__email', 'course_key', 'usage_key']
+
+    def outcome_service_id(self, obj):
+        return str(obj.outcome_service.id)
+
+
 admin.site.register(LtiConsumer, LtiConsumerAdmin)
 admin.site.register(OutcomeService, OutcomeServiceAdmin)
+admin.site.register(LtiUser, LtiUserAdmin)
+admin.site.register(LtiContextId, LtiContextIdAdmin)
+admin.site.register(GradedAssignment, GradedAssignmentAdmin)
