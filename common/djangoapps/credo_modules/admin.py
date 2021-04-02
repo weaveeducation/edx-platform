8904ee885165e750cd27881a9e66257473abccc9
dmitry-viskov <dmitry.viskov@webenterprise.ru>
2021-04-02 16:32:56 +0300

    DEV-845 - Add tracking and report output in CMS django for sibling/cousin copies

--- common/djangoapps/credo_modules/admin.py
+++ common/djangoapps/credo_modules/admin.py
@@ -1,12 +1,18 @@
+import csv
+import datetime
+from collections import OrderedDict
+
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import reverse
 from django import forms
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.utils.safestring import mark_safe
 from .models import RegistrationPropertiesPerOrg, EnrollmentPropertiesPerCourse,\
     Organization, OrganizationType, CourseExcludeInsights, CustomUserRole, TagDescription, EdxApiToken,\
     RutgersCampusMapping, Feature, FeatureBetaTester, CredoModulesUserProfile, CredoStudentProperties, SendScores,\
-    TrackingLogConfig, PropertiesInfo
+    TrackingLogConfig, PropertiesInfo, SiblingBlockUpdateTask, SiblingBlockNotUpdated
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 
 
@@ -112,6 +118,46 @@ class ReadOnlyMixin(object):
     def has_change_permission(self, request, obj=None):
         return False
 
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class ExportCsvMixin:
+
+    def get_csv_title(self, request, field_name):
+        return field_name.replace('_', ' ').upper()
+
+    def get_csv_value(self, request, field_name, obj):
+        return getattr(obj, field_name)
+
+    def export_as_csv(self, request):
+        queryset = self.get_export_csv_queryset(request)
+        now_str = datetime.datetime.now().strftime('%m-%d-%Y_%I_%M_%p')
+
+        field_names = OrderedDict()
+        for field_name in self.list_display:
+            field_names[field_name] = self.get_csv_title(request, field_name)
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=cousin_content_block_{}.csv'.format(now_str)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names.values())
+        for obj in queryset:
+            row = []
+            for field_name in self.list_display:
+                row.append(self.get_csv_value(request, field_name, obj))
+            writer.writerow(row)
+
+        return response
+
+    def changelist_view(self, request, extra_context=None):
+        if 'action' in request.POST and request.POST['action'] == 'export_as_csv':
+            return self.export_as_csv(request)
+        return super(ExportCsvMixin, self).changelist_view(request, extra_context)
+
+    export_as_csv.short_description = "Export as CSV"
+
 
 class CredoModulesUserProfileForm(ReadOnlyMixin, admin.ModelAdmin):
     list_display = ('id', 'user', 'course_id', 'meta')
@@ -137,6 +183,117 @@ class PropertiesInfoForm(ReadOnlyMixin, admin.ModelAdmin):
     search_fields = ['org', 'course_id']
 
 
+class SiblingBlockUpdateTaskForm(ReadOnlyMixin, admin.ModelAdmin):
+    list_display = ('id', 'created', 'task_id', 'initiator', 'status',
+                    'source_course_id', 'source_block_id', 'sibling_course_id', 'sibling_block_id',
+                    'published', 'sibling_block_prev_version')
+    search_fields = ['source_course_id', 'source_block_id', 'sibling_course_id', 'sibling_block_id']
+
+
+class IgnoredFilter(admin.SimpleListFilter):
+
+    title = 'Display'
+    parameter_name = 'display'
+
+    def lookups(self, request, model_admin):
+        return (
+            (1, 'All'),
+            (0, 'Only not ignored'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() is None:
+            self.used_parameters[self.parameter_name] = 0
+        else:
+            self.used_parameters[self.parameter_name] = int(self.value())
+        if self.value() == 1:
+            return queryset
+        return queryset.filter(ignore=False)
+
+    def choices(self, changelist):
+        for lookup, title in self.lookup_choices:
+            yield {
+                'selected': str(self.value()) == str(lookup),
+                'query_string': changelist.get_query_string({self.parameter_name: lookup}),
+                'display': title,
+            }
+
+
+class SiblingBlockNotUpdatedForm(ReadOnlyMixin, ExportCsvMixin, admin.ModelAdmin):
+    list_display = ('id', 'source_course_id', 'source_block_name', 'source_block_parents_path', 'source_block_id',
+                    'sibling_course_id', 'sibling_block_id',
+                    'source_version_published_date', 'source_version_publisher',
+                    'sibling_version_published_date', 'sibling_version_publisher',
+                    'ignore')
+    search_fields = ['source_course_id', 'source_block_id', 'sibling_course_id', 'sibling_block_id']
+    actions = ('set_ignored', 'export_as_csv',)
+    list_filter = (IgnoredFilter,)
+    users_cache = {}
+
+    def set_ignored(self, request, queryset):
+        queryset.update(ignore=True)
+
+    set_ignored.short_description = "Ignore Selected Rows"
+
+    def get_export_csv_queryset(self, request):
+        display = str(request.GET.get('display', 0))
+        if display == '0':
+            queryset = SiblingBlockNotUpdated.objects.filter(ignore=False)
+        else:
+            queryset = SiblingBlockNotUpdated.objects.all()
+        return queryset
+
+    def _get_user_id(self, user_id):
+        if user_id:
+            if user_id in self.users_cache:
+                return self.users_cache.get(user_id)
+
+            try:
+                user = User.objects.get(id=user_id)
+                self.users_cache[user_id] = user.username
+                return user.username
+            except User.DoesNotExist:
+                pass
+        return '-'
+
+    def source_version_publisher(self, obj):
+        return self._get_user_id(obj.source_version_publisher_user_id)
+
+    def sibling_version_publisher(self, obj):
+        return self._get_user_id(obj.sibling_version_publisher_user_id)
+
+    def get_csv_title(self, request, field_name):
+        res = super(SiblingBlockNotUpdatedForm, self).get_csv_title(request, field_name)
+        if field_name in ('source_version_published_date', 'sibling_version_published_date'):
+            res = res + ' (UTC)'
+        return res
+
+    def get_csv_value(self, request, field_name, obj):
+        if not hasattr(request, 'users_cache'):
+            setattr(request, 'users_cache', {})
+        if field_name in ('source_version_publisher', 'sibling_version_publisher'):
+            value = None
+            if field_name == 'source_version_publisher':
+                value = obj.source_version_publisher_user_id
+            elif field_name == 'sibling_version_publisher':
+                value = obj.sibling_version_publisher_user_id
+
+            if not value:
+                return None
+
+            if value in request.users_cache:
+                return request.users_cache[value].username
+
+            try:
+                user = User.objects.get(id=value)
+                request.users_cache[value] = user
+                return user.username
+            except User.DoesNotExist:
+                pass
+        else:
+            return super(SiblingBlockNotUpdatedForm, self).get_csv_value(request, field_name, obj)
+
+
 admin.site.register(RegistrationPropertiesPerOrg, RegistrationPropertiesPerOrgForm)
 admin.site.register(EnrollmentPropertiesPerCourse, EnrollmentPropertiesPerCourseForm)
 admin.site.register(Organization, OrganizationForm)
@@ -153,3 +310,5 @@ admin.site.register(CredoStudentProperties, CredoStudentPropertiesForm)
 admin.site.register(SendScores, SendScoresForm)
 admin.site.register(TrackingLogConfig, TrackingLogConfigForm)
 admin.site.register(PropertiesInfo, PropertiesInfoForm)
+admin.site.register(SiblingBlockUpdateTask, SiblingBlockUpdateTaskForm)
+admin.site.register(SiblingBlockNotUpdated, SiblingBlockNotUpdatedForm)
