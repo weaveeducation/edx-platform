9b5e9c8db6d3d38a2d7ba3ab529e96bca65928e1
dmitry-viskov <dmitry.viskov@webenterprise.ru>
2021-06-16 17:55:57 +0300

    DEV-14 - Restore Previous Version - Unit level in Studio

--- common/lib/xmodule/xmodule/modulestore/mixed.py
+++ common/lib/xmodule/xmodule/modulestore/mixed.py
@@ -826,7 +826,7 @@ class MixedModuleStore(ModuleStoreDraftAndPublished, ModuleStoreWriteBase):
         store = self._verify_modulestore_support(location.course_key, 'delete_item')
         return store.delete_item(location, user_id=user_id, **kwargs)
 
-    def revert_to_published(self, location, user_id):
+    def revert_to_published(self, location, user_id, version_id=None):
         """
         Reverts an item to its last published version (recursively traversing all of its descendants).
         If no published version exists, an InvalidVersionError is thrown.
@@ -837,7 +837,7 @@ class MixedModuleStore(ModuleStoreDraftAndPublished, ModuleStoreWriteBase):
         :raises InvalidVersionError: if no published version exists for the location specified
         """
         store = self._verify_modulestore_support(location.course_key, 'revert_to_published')
-        return store.revert_to_published(location, user_id)
+        return store.revert_to_published(location, user_id, version_id)
 
     def reset_course_to_version(self, course_key, version_guid, user_id):
         """
