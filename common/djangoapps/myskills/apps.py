db5d73fbdb09b5aadbfb241c21289cc9cbdef971
dmitry-viskov <dmitry.viskov@webenterprise.ru>
2021-08-27 16:57:10 +0300

    MySkills API CORS allow

--- common/djangoapps/myskills/apps.py
+++ common/djangoapps/myskills/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MySkillsAppConfig(AppConfig):
     name = 'common.djangoapps.myskills'
     verbose_name = 'MySkills'
+
+    def ready(self):
+        from common.djangoapps.myskills import handlers
