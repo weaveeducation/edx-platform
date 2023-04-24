54a76f245299a8e008bd3bb2d3b250e49bad3721
dmitry-epic <d-epic@epicsoftwaredev.com>
2023-04-25 02:17:29 +0400

    DEV-43 - As Credo CS/ID, I want Courses in Studio to be organized and nested by Org

--- cms/static/js/features_jsx/studio/CourseOrLibraryListing.jsx
+++ cms/static/js/features_jsx/studio/CourseOrLibraryListing.jsx
@@ -5,79 +5,118 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export function CourseOrLibraryListing(props) {
-    const allowReruns = props.allowReruns;
-    const linkClass = props.linkClass;
-    const idBase = props.idBase;
+class CourseOrLibraryListing extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            orgs: [],
+        };
+    }
 
-    const renderCourseMetadata = (item, i) => (
-        <div>
-            <h3 className="course-title" id={`title-${idBase}-${i}`}>{item.display_name}</h3>
-            <div className="course-metadata">
-                <span className="course-org metadata-item">
-                    <span className="label">{gettext('Organization:')}</span>
-                    <span className="value">{item.org}</span>
-                </span>
-                <span className="course-num metadata-item">
-                    <span className="label">{gettext('Course Number:')}</span>
-                    <span className="value">{item.number}</span>
-                </span>
-                { item.run &&
-            <span className="course-run metadata-item">
-                <span className="label">{gettext('Course Run:')}</span>
-                <span className="value">{item.run}</span>
-            </span>
-                }
-                { item.can_edit === false &&
-            <span className="extra-metadata">{gettext('(Read-only)')}</span>
-                }
-            </div>
-        </div>
-    );
+    componentDidMount() {
+        this.orgChangeHandler = this.orgChangeHandler.bind(this);
+        const orgSelector = document.getElementById('orgs-input');
+        if (orgSelector) {
+            $(document).on('orgs_changed', this.orgChangeHandler);
+        }
+    }
 
-    return (
-        <ul className="list-courses">
-            {
-                props.items.map((item, i) =>
-                    (
-                        <li key={i} className="course-item" data-course-key={item.course_key}>
-                            {item.url
-                                ? (
-                                    <a className={linkClass} href={item.url}>
-                                        {renderCourseMetadata(item, i)}
-                                    </a>
-                                )
-                                : renderCourseMetadata(item, i)
-                            }
-                            { item.lms_link && item.rerun_link &&
-              <ul className="item-actions course-actions">
-                  { allowReruns &&
-                <li className="action action-rerun">
-                    <a
-                        href={item.rerun_link}
-                        className="button rerun-button"
-                        aria-labelledby={`re-run-${idBase}-${i} title-${idBase}-${i}`}
-                        id={`re-run-${idBase}-${i}`}
-                    >{gettext('Re-run Course')}</a>
-                </li>
-                  }
-                  <li className="action action-view">
-                      <a
-                          href={item.lms_link}
-                          rel="external"
-                          className="button view-button"
-                          aria-labelledby={`view-live-${idBase}-${i} title-${idBase}-${i}`}
-                          id={`view-live-${idBase}-${i}`}
-                      >{gettext('View Live')}</a>
-                  </li>
-              </ul>
-                            }
-                        </li>
-                    ),
-                )
+    componentWillUnmount() {
+        const orgSelector = document.getElementById('orgs-input');
+        if (orgSelector) {
+            $(document).off('orgs_changed');
+        }
+    }
+
+    orgChangeHandler(e) {
+        const orgs = [];
+        if (arguments.length > 0) {
+            for (let i = 1; i < arguments.length; i++) {
+                orgs.push(arguments[i]);
             }
-        </ul>
-    );
+        }
+        this.setState({
+            orgs,
+        });
+    }
+
+    render() {
+        const { allowReruns } = props;
+        const { linkClass } = props;
+        const { idBase } = props;
+
+        const renderCourseMetadata = (item, i) => (
+            <div>
+                <h3 className="course-title" id={`title-${idBase}-${i}`}>{item.display_name}</h3>
+                <div className="course-metadata">
+                    <span className="course-org metadata-item">
+                        <span className="label">{gettext('Organization:')}</span>
+                        <span className="value">{item.org}</span>
+                    </span>
+                    <span className="course-num metadata-item">
+                        <span className="label">{gettext('Course Number:')}</span>
+                        <span className="value">{item.number}</span>
+                    </span>
+                    {item.run
+                        && (
+                            <span className="course-run metadata-item">
+                                <span className="label">{gettext('Course Run:')}</span>
+                                <span className="value">{item.run}</span>
+                            </span>
+                        )}
+                    {item.can_edit === false
+                        && <span className="extra-metadata">{gettext('(Read-only)')}</span>}
+                </div>
+            </div>
+        );
+
+        return (
+            <ul className="list-courses">
+                {
+                    props.items.map((item, i) => (
+                        ((this.state.orgs.length === 0) || (this.state.orgs.indexOf(item.org) !== -1) || this.props.displayAll) && (
+                            <li key={i} className="course-item" data-course-key={item.course_key}>
+                                {item.url
+                                    ? (
+                                        <a className={linkClass} href={item.url}>
+                                            {renderCourseMetadata(item, i)}
+                                        </a>
+                                    )
+                                    : renderCourseMetadata(item, i)}
+                                {item.lms_link && item.rerun_link
+                                && (
+                                    <ul className="item-actions course-actions">
+                                        {allowReruns
+                                            && (
+                                                <li className="action action-rerun">
+                                                    <a
+                                                        href={item.rerun_link}
+                                                        className="button rerun-button"
+                                                        aria-labelledby={`re-run-${idBase}-${i} title-${idBase}-${i}`}
+                                                        id={`re-run-${idBase}-${i}`}
+                                                    >{gettext('Re-run Course')}
+                                                    </a>
+                                                </li>
+                                            )}
+                                        <li className="action action-view">
+                                            <a
+                                                href={item.lms_link}
+                                                rel="external"
+                                                className="button view-button"
+                                                aria-labelledby={`view-live-${idBase}-${i} title-${idBase}-${i}`}
+                                                id={`view-live-${idBase}-${i}`}
+                                            >{gettext('View Live')}
+                                            </a>
+                                        </li>
+                                    </ul>
+                                )}
+                            </li>
+                        )
+                    ))
+                }
+            </ul>
+        );
+    }
 }
 
 CourseOrLibraryListing.propTypes = {
@@ -86,3 +125,5 @@ CourseOrLibraryListing.propTypes = {
     items: PropTypes.arrayOf(PropTypes.object).isRequired,
     linkClass: PropTypes.string.isRequired,
 };
+
+export { CourseOrLibraryListing };
